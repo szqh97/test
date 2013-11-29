af191ee2773f702ce2f216400ee805d21f29fa31
szqh97 <szqh97@163.com>
2013-11-29 18:52:59 +0800

    time modify

--- video_split.cpp
+++ video_split.cpp
@@ -177,7 +177,7 @@ int video_split_processor::video_split(FILE *fp, off_t pos, video_file_info *p_i
 
             timeval tv;
             unsigned int t_seconds = (p_info->begin_time.tv_usec + fts*1000) / 1000000;
-            tv.tv_usec = (p_info->begin_time.tv_usec + fts*1000) % 1000;
+            tv.tv_usec = (p_info->begin_time.tv_usec + fts*1000) % 1000000;
             tv.tv_sec = p_info->begin_time.tv_sec + t_seconds;
 
             m_shot_begin_time.tv_sec = tv.tv_sec;
@@ -209,14 +209,14 @@ int video_split_processor::video_split(FILE *fp, off_t pos, video_file_info *p_i
             timeval tv; 
             unsigned int t_seconds = p_info->begin_time.tv_usec + (i * fts)*1000 / 1000000;
             tv.tv_sec = p_info->begin_time.tv_sec + (i * fts) / 1000 + t_seconds;
-            tv.tv_usec = p_info->begin_time.tv_usec + (i * fts*1000) % 1000;
+            tv.tv_usec = p_info->begin_time.tv_usec + (i * fts*1000) % 1000000;
             m_shot_end_time.tv_sec = tv.tv_sec;
             m_shot_end_time.tv_usec = tv.tv_usec;
 
             rename_yuv_file();
 
             // next shot begin time
-            tv.tv_usec = (tv.tv_usec + fts * 1000) % 1000;
+            tv.tv_usec = (tv.tv_usec + fts * 1000) % 1000000;
             t_seconds = (tv.tv_usec + fts * 1000) / 1000000;
             tv.tv_sec = tv.tv_sec + t_seconds;
             m_shot_begin_time.tv_sec = tv.tv_sec;
