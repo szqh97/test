d56e4cb787295c9be421c9d9f51108a5b9819c7e
liyun <szqh97@szqh97.(none)>
2013-11-30 14:08:06 +0800

    add file stream test

--- t.cpp
+++ t.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string>
+#include <fstream>
 using namespace std;
 
 
 int main ( int argc, char *argv[] )
 {
+    string videofilename;
+    ifstream vfile("t.out");
+    while (vfile >> videofilename)
+    {
+        cout << videofilename << endl;
+
+    }
+
     long long int t = 12345;
     char name[100];
     sprintf(name, "%06lld",(t/1000));
