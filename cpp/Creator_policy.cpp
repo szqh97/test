52c9f76e9b4fc1f075771a894bfdb97406d1d61d
szqh97 <szqh97@163.com>
2014-01-13 23:02:35 +0800

    add policies code

---
+++ cpp/Creator_policy.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <stdlib.h>
+
+template <class T>
+struct OpnewCreator
+{
+    static T* Create()
+    {
+        return new T;
+    }
+};
+
+template <class T>
+struct MallocCreator
+{
+    static T* Create()
+    {
+        void* buf = malloc(sizeof(T));
+        if (!buf) return 0;
+        return new(buf) T;
+    }
+};
+
+template <class T>
+struct PrototypeCreator
+{
+    PrototypeCreator(T* pObj = 0)
+        :pPrototype_(pObj)
+    {}
+    T* Create()
+    {
+        return pPrototype_ ? pPrototype_->Clone() : 0;
+    }
+    T* GetPrototype() { return pPrototype_; }
+    void SetPrototype(T* pObj) { pPrototype_ = pObj; }
+private:
+    T* pPrototype_;
+};
+
