f544ecd4772ed91f7d94707069a4a956940009f0
szqh97 <szqh97@163.com>
2014-01-14 23:00:02 +0800

    add test policy code

--- cpp/test_policy.cpp
+++ cpp/test_policy.cpp
@@ -10,9 +10,19 @@ class widgetManager: public CreattionPolicy
 
 typedef widgetManager< OpNewCreator<IntPair> > myWidgetMgr;
 
+template < template <class creatPolicy> class cp>
+class wdManager : public cp<IntPair>
+{};
+
+typedef wdManager<OpNewCreator> myWdMgr;
+
 int main ( int argc, char *argv[] )
 {
     myWidgetMgr m;
+    myWdMgr k;
+    IntPair *p = new IntPair;
+    
+    delete p;
 
     return 0;
 }			/* ----------  end of function main  ---------- */
