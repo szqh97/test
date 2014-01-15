4065eacc23e4c290f709c691427805db1c1faca5
szqh97 <szqh97@163.com>
2014-01-15 21:07:32 +0800

    add checking policies

---
+++ cpp/Checking.h
@@ -0,0 +1,17 @@
+#ifndef _CHECKING_H_
+#define _CHECKING_H_
+#include <iostream>
+template <class T> struct NoChecking
+{
+    static void Check(T*) {}
+};
+
+template <class T> struct EnforceNotNULL
+{
+    class NullPointerException: public std::exception {};
+    static void CCheck(T* ptr)
+    {
+        if (!ptr) throw NullPointerException()
+    }
+};
+#endif
