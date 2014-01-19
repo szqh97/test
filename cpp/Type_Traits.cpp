8b1fe566151ebbb2436cb177a7760959d1860564
szqh97 <szqh97@163.com>
2014-01-19 16:45:26 +0800

    detect whether a value is a pointer

--- cpp/Type_Traits.cpp
+++ cpp/Type_Traits.cpp
@@ -7,3 +7,35 @@ OutIt Copy(InIt first, InIt last, OutIt result)
         *result = *first;
 }
 
+struct NullType;
+struct EmptyType {};
+template <typename T>
+class TypeTraits
+{
+    private:
+        template <class U> struct PointerTraits
+        {
+            enum { result = false};
+            typedef NullType PointeeType;
+        };
+        template <class U> struct PointerTraits<U*>
+        {
+            enum {result = true};
+            typedef U PointeeType;
+        };
+    public:
+        enum {isPointer = PointerTraits<T>::result };
+        typedef typename PointerTraits<T>::PointeeType PointeeType;
+
+};
+
+
+int main ( int argc, char *argv[] )
+{
+    TypeTraits<int* > a;
+    cout << a.isPointer << endl;
+
+    TypeTraits<int> b;
+    cout << a.isPointer << endl;
+    return 0;
+}			/* ----------  end of function main  ---------- */
