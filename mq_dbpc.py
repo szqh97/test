5d5b4ced30fe5e2573762c7b125c19748c52c6ff
szqh97 <szqh97@163.com>
2014-01-24 11:21:52 +0800

    fix bug: if no 'messages' in result, alarm

--- mq_dbpc.py
+++ mq_dbpc.py
@@ -17,32 +17,30 @@ query_Q_url = "http://54.219.239.245/api/queues/%2F/tv_ads_query_queue.dead"
 airing_log_Q_url = "http://54.219.239.245/api/queues/%2F/tv_ads_airing_log_queue.dead"
 
 dbpc_interval = "600"
-dbpc_host = "192.168.1.146"
+dbpc_host = "206.99.94.53"
 dbpc_port = "5800"
 dbpc_service = "tvads"
 dbpc_component = "Dead_MQ"
 
-def get_Q_info(username, passwd, url):
+def get_MQ_info(username, passwd, url):
     try:
         http = httplib2.Http()
         http.add_credentials(user, passwd)
         resp, content = http.request(url)
         if resp.status == 200:
             if simplejson.loads(content).has_key("messages"):
-                return True
+                return False
     except Exception, err:
         pass
-    return False
+    return True
 
 def dbpc_fun():
-    detect_Q_cmd = "./heartbeat -h %s -p %s -s %s -c %s" % (dbpc_host, dbpc_port, dbpc_service, dbpc_component)
-    d = get_Q_info(user, passwd, detect_Q_url)
-    q = get_Q_info(user, passwd, query_Q_url)
-    a = get_Q_info(user, passwd, airing_log_Q_url)
+    detect_MQ_cmd = "./heartbeat -h %s -p %s -s %s -c %s" % (dbpc_host, dbpc_port, dbpc_service, dbpc_component)
+    d = get_MQ_info(user, passwd, detect_Q_url)
+    q = get_MQ_info(user, passwd, query_Q_url)
+    a = get_MQ_info(user, passwd, airing_log_Q_url)
     if d and q and a:
-        os.system(detect_Q_cmd)
+        ret = os.system(detect_MQ_cmd)
 
 if __name__ == '__main__':
-    while True:
-        dbpc_fun()
-        time.sleep(int(dbpc_interval))
+    dbpc_fun()
