5111225067cfa7b8de48f40bec3ecc0a4062f012
szqh97 <szqh97@163.com>
2014-01-24 16:03:36 +0800

    add DerivedToFront

---
+++ cpp/DerivedToFront.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "Typelist.h"
+using namespace std;
+
+int main ( int argc, char *argv[] )
+{
+
+    return 0;
+}			/* ----------  end of function main  ---------- */
+
