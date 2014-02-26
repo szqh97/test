486e9233f5b2113482ee9e5e830d4b4ab2743edf
szqh97 <szqh97@163.com>
2014-02-26 10:13:42 +0800

    add functions to get all brands and all spots

--- python/ispot/ispot_spider.py
+++ python/ispot/ispot_spider.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import urllib2
 import BeautifulSoup
+import traceback
 import cPickle
 
 home = "http://www.ispot.tv"
@@ -45,7 +46,7 @@ def get_tv_ad_categories2(categories1_list):
 
 def get_last_categories(categories2_list):
     all_barnds_page_list = {}
-    products_page_list = {}
+    #products_page_list = {}
     for category, url in categories2_list.iteritems():
         res = urllib2.urlopen(url)
         html = res.read()
@@ -53,7 +54,7 @@ def get_last_categories(categories2_list):
 
         # current page can find all brands
         if pos == -1:
-            all_barnds_list.update({category : url + "?view-all=true"})
+            all_barnds_page_list.update({category : url + "?view-all=true"})
 #        else:
 #            soup = BeautifulSoup.BeautifulSoup(html)
 #            spl = soup.body.find('div',{"class":"span3 pull-left"})
@@ -67,23 +68,93 @@ def get_last_categories(categories2_list):
 #                products_page_list.update({'/'.join((category, d.a.text)): home + d.a['href']})
     return all_barnds_page_list
 
-def get_brands(categories_list):
-    pass;
+def get_brands(brands_page_list):
+
+    brands_list = {}
+    for category, url in brands_page_list.iteritems():
+        soup = get_browse(url)
+        spl = soup.body.find('div',{"class":"span3 pull-left"})
+        alldivs = spl.div.findAll('div', {"class":"span3"})
+        alldivs = alldivs[1:]
+        try:
+            for d in alldivs:
+                brands_list.update({'/'.join((category, d.a.text)): home + d.a['href']})
+        except Exception, err:
+            print category, url
+            print traceback.format_exc()
+
+    return brands_list
+
+def get_a_brand_spots(brand, brand_url):
+
+    print "get_a_brand_spots ...", brand_url
+
+    soup = None
+    spots_list = {}
+    res = urllib2.urlopen(brand_url)
+    html = res.read()
+    pos = html.find("Show all Commercials")
+
+    # if you can view all commercials
+    if pos != -1:
+        res = urllib2.urlopen(brand_url + "?view-all=true")
+        html = res.read()
+    else:
+        soup = BeautifulSoup.BeautifulSoup(html)
+        clearfix = soup.find("div", {"class":"clearfix"})
+        divs = clearfix.findAll("div")
+        for d in divs:
+            if d.a:
+                spot = brand + "/" + d.a.text
+                spot_url = home + d.a.get("href")
+                spots_list.update({spot:spot_url})
+
+    if not soup:
+        soup = BeautifulSoup.BeautifulSoup(html)
+    span2s = soup.findAll("div", {"class":"span2"})
+    for d in span2s:
+        if d.a:
+            href = d.a.get("href")
+            if href:
+                spot = brand + "/" + d.a.text
+                spot_url = home + href
+                spots_list.update({spot:spot_url})
+    return spots_list
+
+def get_all_spots(brand_list):
+    print "get_all_spots ...."
+    all_spots = {}
+    for category, url in brand_list.iteritems():
+        spots = get_a_brand_spots(category, url)
+        all_spots.update(spots)
+    return all_spots
+
+
 
 if __name__ == '__main__':
-    soup = get_browse(browseurl)
-    print "a"
-    c1 = get_tv_ad_categories1(soup)
-    print c1
-    c2 = get_tv_ad_categories2(c1)
-    print len(c2)
-    print c2
-    os.exit(0)
-    all_brands, products = get_last_categories(c2)
-    with file('call_brands.pk', 'w') as f:
-        cPickle.dump(all_brands, f)
-    with file('products.pk', 'w') as f:
-        cPickle.dump(products, f)
+#    soup = get_browse(browseurl)
+#    print "a"
+#    c1 = get_tv_ad_categories1(soup)
+#    print c1
+#    c2 = get_tv_ad_categories2(c1)
+#    print len(c2)
+#    print c2
+#    all_brands_page = get_last_categories(c2)
+#    with file('all_brands_page.pk', 'w') as f:
+#        cPickle.dump(all_brands_page, f)
+#    with file ('all_brands_page.pk', 'r') as f:
+#        all_brands_page_list = cPickle.load(f)
+#    all_brands = get_brands(all_brands_page_list)
+#    with file ('all_brands.pk', 'w') as p:
+#        cPickle.dump(all_brands, p)
+    all_brands = None
+    with file ('all_brands.pk', 'r') as f:
+        all_brands = cPickle.load(f)
+    all_spots = get_all_spots(all_brands)
+
+    with file ('all_spots.pk', 'w') as f:
+        cPickle.dump(all_spots, f)
+    
 
 
 
