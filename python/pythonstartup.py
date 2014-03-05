a8429b70f1e313c4be725325171eb23634f3d0a3
szqh97 <szqh97@163.com>
2014-03-05 09:44:37 +0800

    add pythonstartup.py; by this file we can use python cmd as ipython

---
+++ python/pythonstartup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+"""
+USAGE: rename this file to .pythonstartup
+set PYTHONSTARTUP
+export PYTHONSTARTUP=~/.pythonstartup
+"""
+import readline
+import rlcompleter
+import atexit
+import os
+#tab completion
+readline.parse_and_bind('tab: complete')
+# history file
+hisfile = os.path.join(os.environ['HOME'], '.pythonhistory')
+try:
+    readline.read_history_file(hisfile)
+except IOError:
+    pass
+atexit.register(readline.write_history_file, histfile)
+del os, histfile, readline, rlcompleter
