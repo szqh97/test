7a3f36c64ba0b3cc15f37280f9744cd17ba04ee2
szqh97 <szqh97@163.com>
2014-03-17 11:31:35 +0800

    add file

---
+++ go/filetest.go
@@ -0,0 +1,17 @@
+package main
+import (
+    "./file"
+    "fmt"
+    "os"
+)
+
+func main() { 
+    hello := []byte("hello world\n")
+    file.Stdout.Write(hello)
+    file, err := file.Open("/does/not/exist", 0, 0)
+    if file == nil {
+        fmt.Println("can't open file: err= ", err)
+        os.Exit(1)
+    }
+}
+
