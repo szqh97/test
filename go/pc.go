eba67460dbf0cb736230f0ce66717f4460ab5e43
szqh97 <szqh97@163.com>
2014-03-25 22:17:45 +0800

    add pc.go

--- go/pc.go
+++ go/pc.go
@@ -7,7 +7,7 @@ import (
 //    "reflect"
    // "io/ioutil"
  //   "encoding/gob"
-   "bytes"
+//   "bytes"
     "os"
     //"log"
 )
@@ -30,30 +30,50 @@ type dna struct {
     Pad [36]byte
 }
 
-func (cb *control_block) read_cb(cb_buf []byte) error {
+func (cb *control_block) Parse_cb(cb_buf []byte) error {
     if len(cb_buf) != 24 {
         return errors.New("length of cb_buf error")
     }
     cb.Ser_num = binary.LittleEndian.Uint32(cb_buf[0:4])
     cb.Dna_type = binary.LittleEndian.Uint32(cb_buf[4:8])
     cb.Length = binary.LittleEndian.Uint32(cb_buf[8:12])
-    cb.pad = binary.LittleEndian.Uint32(cb_buf[12:16])
+    cb.Pad = binary.LittleEndian.Uint32(cb_buf[12:16])
     cb.Ts = binary.LittleEndian.Uint64(cb_buf[16:24])
     return nil
 }
 
-func (d *dna) read_dna(dna_buf []byte) error {
+func (d *dna) Parse_dna(dna_buf []byte) error {
     if len(dna_buf) != 40 {
         return errors.New("length of dna_frame error")
     }
     d.Ts = binary.LittleEndian.Uint32(dna_buf[0:4])
-    copy(d.Pad, dna_buf[4:40])
     return nil
 }
 
-func extract_ts(file string) error {
-    file := os.Open(file)
+func extract_ts(filename string) error {
+    file, err := os.Open(filename)
+    if err != nil {
+
+    }
     file.Seek(24+48, os.SEEK_SET)
+    cb_buf := make([]byte, 24)
+    var cb control_block
+    var dna_frame dna
+    file.Read(cb_buf)
+    cb.Parse_cb(cb_buf)
+    dna_buf := make([]byte, cb.Length)
+    for cb.Length != 0 {
+        file.Read(dna_buf)
+        for i := 0; i < int(cb.Length/40); i++ {
+            dna_frame.Parse_dna(dna_buf[i*40: (i+1)*40])
+            fmt.Println(dna_frame.Ts)
+        }
+        file.Read(cb_buf)
+        cb.Parse_cb(cb_buf)
+    }
+
+    file.Close()
+    return nil
 
 }
 
@@ -61,16 +81,7 @@ func main() {
     //l := intDataSize(dnas)
 
     dnafile := "./16.1394089199.cdna"
-    file, err := os.Open(dnafile)
-    if err != nil {
-        fmt.Println("xx", err)
-
-    }
-    cb_buf := make([]byte, 240)
-    //file.Seek(24+48, os.SEEK_SET)
-    file.Read(cb_buf)
-    r := bytes.NewReader(cb_buf)
-    cb, err := read_cb(r)
+    extract_ts(dnafile)
    //dnafile := flag.NArg(1)
     //extract_ts(dnafile)
 }
