72124e7ba8d6df8eae204b34d2c068346cd44f5b
szqh97 <szqh97@163.com>
2014-03-25 22:21:18 +0800

    update test.go

--- go/test.go
+++ go/test.go
@@ -1,9 +1,15 @@
 package main
 import "fmt"
+import "os"
 
 var a interface{}
 func f() interface{} {
     return 444
+}
+func read (ch chan []byte) {
+    buf := make ([]byte, 128)
+    buf <-ch
+    fmt.Println(buf)
 }
 func main() {
     var i int = 5
@@ -13,5 +19,17 @@ func main() {
     a = s
     fmt.Println(a)
     fmt.Println(f())
+    rn :=rune(i)
+    fmt.Println(uint32(rn))
+    ch := make(chan []byte, 1024)
+    file, err := os.Open("./pc.go")
+    if err != nil {
+    }
+    buf := make([]byte, 128)
+    file.Read(buf)
+    ch<-buf
+    
+
 }
+
 
