60404f68fc7a44526cb9f8f5f5b940256b5c011d
szqh97 <szqh97@163.com>
2014-03-26 09:34:09 +0800

    format pc.go

--- go/pc.go
+++ go/pc.go
@@ -1,24 +1,26 @@
 package main
+
 import (
-    "flag"
-    "errors"
-    "encoding/binary"
-    "fmt"
-//    "reflect"
-   // "io/ioutil"
- //   "encoding/gob"
-//   "bytes"
-    "os"
-    //"log"
+	"encoding/binary"
+	"errors"
+	"flag"
+	"fmt"
+	//    "reflect"
+	// "io/ioutil"
+	//   "encoding/gob"
+	//   "bytes"
+	"os"
+	//"log"
 )
 
 type control_block struct {
-    Ser_num uint32
-    Dna_type uint32
-    Length uint32
-    Pad uint32
-    Ts uint64
+	Ser_num  uint32
+	Dna_type uint32
+	Length   uint32
+	Pad      uint32
+	Ts       uint64
 }
+
 /*
 type dna_heaer struct {
     pad [48]byte
@@ -26,65 +28,67 @@ type dna_heaer struct {
 */
 
 type dna struct {
-    Ts uint32
-    Pad [36]byte
+	Ts  uint32
+	Pad [36]byte
 }
 
 func (cb *control_block) Parse_cb(cb_buf []byte) error {
-    if len(cb_buf) != 24 {
-        return errors.New("length of cb_buf error")
-        fmt.Println('a')
-    }
-    cb.Ser_num = binary.LittleEndian.Uint32(cb_buf[0:4])
-    cb.Dna_type = binary.LittleEndian.Uint32(cb_buf[4:8])
-    cb.Length = binary.LittleEndian.Uint32(cb_buf[8:12])
-    cb.Pad = binary.LittleEndian.Uint32(cb_buf[12:16])
-    cb.Ts = binary.LittleEndian.Uint64(cb_buf[16:24])
-    return nil
+	if len(cb_buf) != 24 {
+		return errors.New("length of cb_buf error")
+		fmt.Println('a')
+	}
+	cb.Ser_num = binary.LittleEndian.Uint32(cb_buf[0:4])
+	cb.Dna_type = binary.LittleEndian.Uint32(cb_buf[4:8])
+	cb.Length = binary.LittleEndian.Uint32(cb_buf[8:12])
+	cb.Pad = binary.LittleEndian.Uint32(cb_buf[12:16])
+	cb.Ts = binary.LittleEndian.Uint64(cb_buf[16:24])
+	return nil
 }
 
 func (d *dna) Parse_dna(dna_buf []byte) error {
-    if len(dna_buf) != 40 {
-        return errors.New("length of dna_frame error")
-    }
-    d.Ts = binary.LittleEndian.Uint32(dna_buf[0:4])
-    return nil
+	if len(dna_buf) != 40 {
+		return errors.New("length of dna_frame error")
+	}
+	d.Ts = binary.LittleEndian.Uint32(dna_buf[0:4])
+	return nil
 }
 
 func extract_ts(filename string) error {
-    file, err := os.Open(filename)
-    if err != nil {
+	file, err := os.Open(filename)
+	if err != nil {
 
-    }
-    file.Seek(24+48, os.SEEK_SET)
-    cb_buf := make([]byte, 24)
-    var cb control_block
-    var dna_frame dna
-    file.Read(cb_buf)
-    cb.Parse_cb(cb_buf)
-    dna_buf := make([]byte, cb.Length)
-    for cb.Length != 0 {
-        file.Read(dna_buf)
-        for i := 0; i < int(cb.Length/40); i++ {
-            dna_frame.Parse_dna(dna_buf[i*40: (i+1)*40])
-            //fmt.Println(dna_frame.Ts)
-        }
-        file.Read(cb_buf)
-        cb.Parse_cb(cb_buf)
-    }
+	}
+	file.Seek(24+48, os.SEEK_SET)
+	cb_buf := make([]byte, 24)
+	var (
+		cb        control_block
+		dna_frame dna
+	)
+	file.Read(cb_buf)
+	cb.Parse_cb(cb_buf)
+	dna_buf := make([]byte, cb.Length)
+	for cb.Length != 0 {
+		file.Read(dna_buf)
+		for i := 0; i < int(cb.Length/40); i++ {
+			dna_frame.Parse_dna(dna_buf[i*40 : (i+1)*40])
+			//fmt.Println(dna_frame.Ts)
+		}
+		file.Read(cb_buf)
+		cb.Parse_cb(cb_buf)
+	}
 
-    file.Close()
-    return nil
+	file.Close()
+	return nil
 
 }
 
 func main() {
-    //l := intDataSize(dnas)
+	//l := intDataSize(dnas)
 
-    //dnafile := "./16.1394089199.cdna"
-    flag.Parse()
-    dnafile := flag.Arg(0)
-    fmt.Println(dnafile)
-    extract_ts(dnafile)
-    //extract_ts(dnafile)
+	//dnafile := "./16.1394089199.cdna"
+	flag.Parse()
+	dnafile := flag.Arg(0)
+	fmt.Println(dnafile)
+	extract_ts(dnafile)
+	//extract_ts(dnafile)
 }
