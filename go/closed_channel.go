a814d82c583f365d1c43f19918c77c45cc356646
szqh97 <szqh97@163.com>
2014-03-26 17:32:13 +0800

    add close_channel test code

--- go/closed_channel.go
+++ go/closed_channel.go
@@ -7,18 +7,21 @@ import (
 
 
 func main() {
+    const n = 1
     finish := make(chan bool)
     var done sync.WaitGroup
-    done.Add(1)
-    go func(){
-        select {
-        case <- time.After(1 * time.Hour):
-        case <- finish:
-        }
-        done.Done()
-    }()
+    for i := 0; i < n; i++ {
+        done.Add(1)
+        go func(){
+            select {
+            case <- time.After(1 * time.Second):
+            case <- finish:
+            }
+            done.Done()
+        }()
+    }
     t0 := time.Now()
-    finish <- true
+    close(finish)
     done.Wait()
     fmt.Printf("waited %v for goroutine to stop\n", time.Since(t0))
 }
