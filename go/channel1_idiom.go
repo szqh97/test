c429a6d8444e5f29a7249cc8180e5f2c46da75cf
szqh97 <szqh97@163.com>
2014-04-02 16:14:38 +0800

    add code

---
+++ go/channel1_idiom.go
@@ -0,0 +1,25 @@
+package main
+import (
+    "fmt"
+    "time"
+)
+func pump () <- chan int {
+    ch := make (chan int)
+    go func() {
+        for i := 0; ; i++ {
+            ch <- i
+        }
+    }()
+    return ch
+}
+func suck (ch <- chan int) {
+    go func(){
+        for v:= range ch {
+            fmt.Println(v)
+        }
+    }()
+}
+func main() {
+    suck(pump())
+    time.Sleep(1e9)
+}
