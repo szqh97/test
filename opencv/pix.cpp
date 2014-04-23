1a5dec66b326ef30ca775eaf3d009c699bf423a0
szqh97 <szqh97@163.com>
2014-04-23 17:51:55 +0800

    use hist to get the pos

--- opencv/pix.cpp
+++ opencv/pix.cpp
@@ -16,6 +16,11 @@ int main ( int argc, char *argv[] )
     }
     image = imread(argv[1], 0);
 
+    int *hist_w = new int[image.cols];
+    int *hist_h = new int[image.rows];
+    memset(hist_h, 0, image.cols * sizeof(int));
+    memset(hist_w, 0, image.rows * sizeof(int));
+
     if (!image.isContinuous()) 
     {
     }
@@ -42,7 +47,6 @@ int main ( int argc, char *argv[] )
     uchar *data =(uchar*)Iimpl.imageData;
     int step = Iimpl.widthStep/sizeof(uchar);
 
-    vector<int> rpos;
     {
         uchar pprev =0, pcur = 0;
         for (int r = 0; r < I.rows; ++r)
@@ -51,16 +55,62 @@ int main ( int argc, char *argv[] )
             for (int c = 0; c <I.cols; ++c)
             {
                 pcur |= I.at<uchar>(r, c);
+                if (I.at<uchar>(r,c) >0)
+                {
+                    ++hist_w[c];
+                    ++hist_h[r];
+                }
                 //pcur |=  uchar(data[r * I.cols  +c]);
             }
-            if (pprev != pcur)
-            {
-                rpos.push_back(r);
-            }
-            pprev = pcur;
         }
     }
+    unsigned long long sum = 0;
+    for (int i=0; i < image.rows; ++i)
+    {
+        sum += hist_w[i];
+    }
+    for(int i = 0; i < image.rows; ++i)
+    {
+        if(hist_w[i] > sum/image.rows)
+            hist_w[i] = 1;
+        else
+            hist_w[i] = 0;
+    }
+    sum = 0;
+    for (int i=0; i < image.cols; ++i)
+    {
+        sum += hist_h[i];
+    }
+    for(int i = 0; i < image.cols; ++i)
+    {
+        if(hist_h[i] > sum/image.cols)
+            hist_h[i] = 1;
+        else
+            hist_h[i] = 0;
+    }
+    cout << endl;
+    {
+        for (int i=0; i < image.rows-1; ++i)
+        {
 
+            if(hist_h[i] != hist_h[i+1])
+            {
+                line(image, Point(0, i), Point(image.cols -1, i), Scalar(0,0,255), 1, CV_AA);
+            }
+        
+        }
+    }
+    {
+        for (int i=0; i < image.cols-1; ++i)
+        {
+            if(hist_w[i] != hist_w[i+1])
+            {
+                line(image, Point(i, 0), Point(i, image.rows-1 ), Scalar(0,0,255), 1, CV_AA);
+            }
+
+        }
+    } 
+#if 0 
     vector<int> cpos;
     {
         uchar pprev =0, pcur = 0;
@@ -79,8 +129,9 @@ int main ( int argc, char *argv[] )
             pprev = pcur;
         }
     }
+#endif
 
-#if 1
+#if 0
     for (int i = 0; i < rpos.size(); i+=2)
     {
 
@@ -92,7 +143,7 @@ int main ( int argc, char *argv[] )
             //rects.insert(pair<Point, Point>(Point(rpos[i], cpos[j]), Point(rpos[i+1], cpos[j+1])));
             cout << p1 << "|" <<p2 <<endl;
             rectangle(image, p1, p2, Scalar(0,0,255), 0, 8);
-            
+
         }
     }
 #endif 
@@ -104,7 +155,7 @@ int main ( int argc, char *argv[] )
 #if 0
     for (size_t i = 0;  i < rpos.size(); ++i)
     {
-            cout << rpos[i] << endl;
+        cout << rpos[i] << endl;
 
     }
 
