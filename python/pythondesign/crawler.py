00063e48443b0601b153437d187e0d954627c644
szqh97 <szqh97@163.com>
2014-05-18 22:15:50 +0800

    add python design pattern code

---
+++ python/pythondesign/crawler.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+import httplib2
+import os
+import threading
+import urllib
+from urlparse import urlparse, urljoin
---
+++ python/pythondesign/singleton.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+class Singelton(object):
+    def __new__(cls):
+        if not hasattr(cls, "instance"):
+            cls.instance = super(Singelton, cls).__new__(cls)
+        return cls.instance
+
+class Borg(object):
+    _shared_state = {}
+
+    def __new__(cls, *args, **kwargs):
+        obj = super(Borg, cls).__new__(cls, *args, **kwargs)
+        obj.__dict__ = cls._shared_state
+        return obj
