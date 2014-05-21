aa559d60719bc9a0c808a9ca5066bcd9513336a9
szqh97 <szqh97@163.com>
2014-05-21 18:09:13 +0800

    update code

--- python/pythondesign/Factory_Method.py
+++ python/pythondesign/Factory_Method.py
@@ -59,3 +59,68 @@ class HTTPConnector(Connector):
             filenames.append(link['href'])
         return '\n'.join(filenames)
 
+
+class FTPConnector(Connector):
+    """ A concrete createor that creates a FTP connector and sets in  
+    runtime all its attributes. """
+
+    def protocal_factory_method(self):
+        return "ftp"
+
+    def port_factory_method(self):
+        return FTPPort()
+
+    def parse(self, content):
+        lines = content.seplit('\n')
+        filenames = []
+        for line in lines:
+            #The ftp fromat typically has a 8colums, split them 
+            splited_line = line.split(None, 8)
+            if len(splited_line) == 9:
+                filenames.append(splited_line[-1])
+
+        return '\n'.joing(filenames)
+
+class Port(object):
+    __metaclass__ = abc.ABCMeta
+    """ Abstract product. one of its subclasses will be created in factory methods. """
+
+    @abc.abstractmethod
+    def __str__(self):
+        pass
+
+class HTTPPort(Port):
+    """ A concrete product which represents http port. """
+
+    def __str(self):
+        return "80"
+
+class HTTPSecureProt(Port):
+    """ a CONCRETE product which represents https port."""
+    def __str__(self):
+        return "443"
+
+class FTPPort(self):
+    """ A concrete product which represents ftp port."""
+    def __str__(self):
+        return "21"
+
+if __name__ == '__main__':
+    domain = 'ftp.freebsd.org'
+    path = '/pub/FreeBSD/'
+
+    protocol = input("Connecting to {}, which Protocol to use? (0-http, 1-ftp): ".format(domain))
+
+    if protocol == 0:
+        is_secure = bool(input("Use secure connection? (1-yes, 0-no): "))
+        connector = HTTPConnector(is_secure)
+    else:
+        is_secure = False
+        connector = FTPConnector(is_secure)
+
+    try:
+        content = connector.read(domain, path)
+    except urllib2.URLError, e:
+        print "Can not access resource with this method"
+    else:
+        print connector.parse(content)
