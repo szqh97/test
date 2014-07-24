801753a4d01843f06457b1f09cc481e12e9b530b
szqh97 <szqh97@163.com>
2014-07-24 16:09:23 +0800

    updte recorder.sh

--- shell/recorder.sh
+++ shell/recorder.sh
@@ -10,6 +10,13 @@ do
         ps -ef|grep "mplayer -dumpstream http://122.192.67.86:8088/t" |grep -v grep |awk '{print $2}'|xargs kill -9
         mplayer -dumpstream http://122.192.67.86:8088/ts300  -dumpfile ${start_ts}.mp4 >${start_ts}.mp4.log 2>&1 &
     else
+
+        pid=$(ps -ef|grep "mplayer -dumpstream http://122.192.67.86:8088/t" |grep -v grep |awk '{print $2}')
+        if [ "$pid" = "" ]
+        then
+            new_start_ts=$(date +%s) 
+            mplayer -dumpstream http://122.192.67.86:8088/ts300  -dumpfile ${new_start_ts}.mp4 >./log/${new_start_ts}.mp4.log 2>&1 &
+        fi
         sleep 30
     fi 
 done
