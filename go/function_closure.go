d1c35393ebf4c7618024d39bd85c2855eb19f2f5
szqh97 <szqh97@163.com>
2014-08-25 10:54:46 +0800

    add function_closure.go

---
+++ go/function_closure.go
@@ -0,0 +1,16 @@
+package main
+import "fmt"
+func main() {
+    var f = Adder()
+    fmt.Print(f(1), " - ")
+    fmt.Print(f(20), " - ")
+    fmt.Print(f(300), "\n")
+}
+
+func Adder() func(int) int{
+    var x int
+    return func(delta int) int{
+        x += delta
+        return x
+    }
+}
