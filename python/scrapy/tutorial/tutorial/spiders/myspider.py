a93d78b42aba56b2b350629fb670a84db7206f4a
szqh97 <szqh97@163.com>
2014-10-14 08:18:27 +0800

    update

--- python/scrapy/tutorial/tutorial/spiders/myspider.py
+++ python/scrapy/tutorial/tutorial/spiders/myspider.py
@@ -1,6 +1,7 @@
 import scrapy
 from scrapy import log
 from tutorial.items import MyItem
+from scrapy.contrib.loader import ItemLoader
 class Myspider(scrapy.Spider):
     name = "myspider"
     allowed_domains = ['douban.com']
@@ -8,14 +9,17 @@ class Myspider(scrapy.Spider):
         super(Myspider, self).__init__(*args, **kwargs)
         self.allowed_domains = ['douban.com']
         self.start_urls = ['http://music.douban.com/%s/' % category,
-                'http://www.baidu.com'
             ]
 
+#    def parse(self, response):
+#        self.log('xxxxxxxxxxxxxxxxxx the response url is : %s' % response.url)
+#        sel = scrapy.Selector(response)
+#        for title in response.xpath('''//div[@class="carousel"]/div[@class="songlist-slides slide-page"]/ul[@class="list-songlist slide-item"]/li[@class="songlist-item"]/a[@class="lnk-songlist"]/@title''').extract():
+#            yield MyItem(title=title)
+#        for url in response.xpath('''//div[@class="carousel"]/div[@class="songlist-slides slide-page"]/ul[@class="list-songlist slide-item"]/li[@class="songlist-item"]/a[@class="lnk-songlist"]/@href''').extract():
+#            yield scrapy.Request(url, callback=self.parse)
+#
     def parse(self, response):
-        self.log('xxxxxxxxxxxxxxxxxx the response url is : %s' % response.url)
-        sel = scrapy.Selector(response)
-        for title in response.xpath('''//div[@class="carousel"]/div[@class="songlist-slides slide-page"]/ul[@class="list-songlist slide-item"]/li[@class="songlist-item"]/a[@class="lnk-songlist"]/@title''').extract():
-            yield MyItem(title=title)
-        for url in response.xpath('''//div[@class="carousel"]/div[@class="songlist-slides slide-page"]/ul[@class="list-songlist slide-item"]/li[@class="songlist-item"]/a[@class="lnk-songlist"]/@href''').extract():
-            yield scrapy.Request(url, callback=self.parse)
-
+        l = ItemLoader(item = MyItem(), response = response)
+        l.add_xpath ('title', '''//div[@class="carousel"]/div[@class="songlist-slides slide-page"]/ul[@class="list-songlist slide-item"]/li[@class="songlist-item"]/a[@class="lnk-songlist"]/@title''')
+        return l.load_item()
