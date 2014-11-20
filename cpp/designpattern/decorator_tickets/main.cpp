cc290f957161215c9b3ff625b09e7ad039d63b79
szqh97 <szqh97@163.com>
2014-11-20 10:01:45 +0800

    add Factory in patterns

--- cpp/designpattern/decorator_tickets/main.cpp
+++ cpp/designpattern/decorator_tickets/main.cpp
@@ -2,15 +2,41 @@
 #include <iostream>
 using namespace std;
 
-int main ( int argc, char *argv[] )
+class Factory
 {
+    public:
+        Factory ();  
+        ~Factory ();
+        Component* getComponent();
+
+    protected:
+    private:
+
+};
+
+Factory::Factory(){}
+
+Factory::~Factory(){}
+
+Component* Factory::getComponent()
+{ 
+
     SalesTicket* ticket = new SalesTicket();
     TicketDecrator* pdec = new Footer1(ticket);
     pdec = new Footer2(pdec);
     pdec = new Header1(pdec);
     pdec = new Header2(pdec);
-    pdec->prtTicket();
-    delete pdec;
+
+    return pdec;
+}
+
+
+int main ( int argc, char *argv[] )
+{
+    Factory fact = Factory();
+    Component* cp = fact.getComponent();
+    cp->prtTicket();
+    delete cp;
 
     return 0;
 }			/* ----------  end of function main  ---------- */
