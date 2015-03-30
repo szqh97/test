0e9c741e5b2dbe249b319990a202cad86fc52400
szqh97 <szqh97@163.com>
2015-03-30 14:55:51 +0800

    update

--- cpp/c11/test_thread_new.cpp
+++ cpp/c11/test_thread_new.cpp
@@ -13,6 +13,7 @@ void f()
 int main ( int argc, char *argv[] )
 {
     std::thread t(f);
+    cout << t.get_id() << endl;
     t.detach();
     this_thread::sleep_for(chrono::milliseconds(3000));
     cout << "int main thread..." << endl;
