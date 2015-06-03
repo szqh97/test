6061d8f0ee53b665bea3eb6a7c67007553f47777
szqh97 <szqh97@163.com>
2015-06-03 22:47:03 +0800

    add string_service.py

---
+++ python/tornado/string_service.py
@@ -0,0 +1,36 @@
+import textwrap
+import tornado.httpserver
+import tornado.ioloop
+import tornado.web
+import tornado.options
+
+from tornado.options import define, options
+define('port', default = 8000, help = 'run on the given port', type =int)
+
+class ReverseHandler(tornado.web.RequestHandler):
+    def get(self, input):
+        self.write(input[::-1])
+
+class WrapHandler(tornado.web.RequestHandler):
+    def post(self):
+        text = self.get_argument('text')
+        width = self.get_argument('width',40)
+        self.write(textwrap.fill(text, int(width)))
+
+handlers = [
+        (r'/reverse/(\w+)', ReverseHandler),
+        (r'/wrap', WrapHandler)
+        ]
+
+# for post request: 
+# curl http://localhost:8000/wrap -d text=Lorem+ipsum+dolor+sit+amet,+consectetuer+adipiscing+elit.
+if __name__ == '__main__':
+    tornado.options.parse_command_line()
+    app = tornado.web.Application(handlers = handlers)
+
+    http_server = tornado.httpserver.HTTPServer(app)
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
+
+
+
