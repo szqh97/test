72bb040706e1da263ea54650f235fa88026a5376
szqh97 <szqh97@163.com>
2015-06-03 14:00:57 +0800

    add test codes for python

---
+++ python/91advices/const.py
@@ -0,0 +1,13 @@
+class _const:
+    class ConstError(TypeError): pass
+    class ConstCaseError(ConstError): pass
+    def __setattr__(self, name, value):
+        if self.__dict__.has_key(name):
+            raise self.ConstError, "Can't change const.%s " % name
+        if not name.isupper():
+            raise self.ConstCaseError, 'const name "%s" is not all upper case ' % name
+        self.__dict__[name] = value
+
+import sys
+sys.modules[__name__] = _const()
+
