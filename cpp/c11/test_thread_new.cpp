4bbc907b34e9e258a67dc42c2ba5f8733c29df94
szqh97 <szqh97@163.com>
2015-06-15 10:25:15 +0800

    add std::thread::id test code

--- cpp/c11/test_thread_new.cpp
+++ cpp/c11/test_thread_new.cpp
@@ -15,8 +15,17 @@ int main ( int argc, char *argv[] )
     std::thread t(f);
     cout << t.get_id() << endl;
     t.detach();
-    this_thread::sleep_for(chrono::milliseconds(3000));
+    this_thread::sleep_for(chrono::milliseconds(300));
     cout << "int main thread..." << endl;
+    thread::id id1 = thread::id();
+
+    thread::id id2 = thread::id();
+    if (id1 == id2) 
+    {
+        cout << "xxxxxxxxxxx" << endl;
+    }
+    cout << std::this_thread::get_id() << endl;
+    cout << std::thread::id() << endl;
 
 
     return 0;
