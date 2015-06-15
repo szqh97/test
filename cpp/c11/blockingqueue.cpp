17e9fbaff5a294cfb4fda766d64817b92688f981
szqh97 <szqh97@163.com>
2015-06-15 16:55:21 +0800

    add blockingqueue for test condition variable"

---
+++ cpp/c11/blockingqueue.cpp
@@ -0,0 +1,68 @@
+#include <mutex>
+#include <queue>
+#include <thread>
+#include <assert.h>
+#include <iostream>
+#include <condition_variable>
+
+std::mutex mutex_;
+//std::unique_lock<std::mutex> lock(mutex_);
+std::condition_variable cv;
+std::deque<int> queu;
+int dequeue()
+{
+    std::unique_lock<std::mutex> lock(mutex_);
+    while(queu.empty())
+    {
+        cv.wait(lock);
+    }
+
+    assert(!queu.empty());
+    int top = queu.front();
+    queu.pop_front();
+    return top;
+
+}
+
+void enqueue(int x)
+{
+    {
+        std::unique_lock<std::mutex> lock(mutex_);
+        queu.push_back(x);
+    }
+    cv.notify_one();
+}
+
+void func1()
+{
+
+    for (int i = 0; i < 100; ++i)
+    {
+        enqueue(i);
+        std::cout << "in enqueue .." << std::endl;
+    }
+
+}
+
+
+void func2()
+{
+    for(int i = 0; i< 100; ++i)
+    {
+        std::cout << dequeue() << std::endl;
+    }
+
+}
+int main ( int argc, char *argv[] )
+{
+
+    std::thread t2(func2);
+    std::thread t1(func1);
+
+    t2.join();
+    t1.join();
+    while (1)
+        std::this_thread::sleep_for(std::chrono::microseconds(1000));
+    return 0;
+}			/* ----------  end of function main  ---------- */
+
