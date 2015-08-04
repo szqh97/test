bb674b3392a936a811ffdef7f1cd6661f982bac7
szqh97 <szqh97@163.com>
2015-08-04 09:54:59 +0800

    update trees.py

--- python/machine_learning/trees.py
+++ python/machine_learning/trees.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from math import log
+import operator
 def calcShannonEnt(dataSet):
     numEntries = len(dataSet)
     labelCounts = {}
@@ -52,4 +53,34 @@ def chooseBestFeatureToSplit(dataSet):
             bestInfoGain = infoGain
             bestFeature = i
     return bestFeature
+
+def majorityCnt(classList):
+    classCont={}
+    for vote in classList:
+        if vote not in classCont.keys():
+            classCount[vote] = 0
+        classCount[vote] += 1
+    sortedClassCount = sorted(classCount.iteritems(), \
+            key=operator.itemgetter(i), reverse=True)
+    return sortedClassCount
+
+
+def createTree(dataSet, labels):
+    classList = [example[-1] for example in dataSet]
+    if classList.count(classList[0]) == len(classList):
+        return classList[0]
+    if len(dataSet[0]) == 1:
+        return majorityCnt(classList)
+    bestFeat = chooseBestFeatureToSplit(dataSet)
+    bestFeatLabel = labels[bestFeat]
+    myTree = {bestFeatLabel:{}}
+    del labels[bestFeat]
+
+    featValues = [example[bestFeat] for example in dataSet]
+    uniqueVals = set(featValues)
+    for value in uniqueVals:
+        subLabels = labels[:]
+        myTree[bestFeatLabel][value] = createTree(splitDataSet(dataSet,\
+                bestFeat, value), subLabels)
+    return myTree
 
