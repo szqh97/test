48e8395725197406939081d2ec39dbe06febeb7b
szqh97 <szqh97@163.com>
2015-08-04 09:56:36 +0800

    add treeplotter.py

---
+++ python/machine_learning/treePlotter.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+import matplotlib.pyplot as plt
+decisionNode = dict(boxstyle="sawtooth", fc="0.8")
+leafNode = dict(boxstyle="round4", fc="0.8")
+arrow_args = dict(arrowstyle="<-")
+
+def plotNode(nodeTxt, centerPt, parentPt, nodeType):
+    createPlot.ax1.annotate(nodeTxt, xy=parentPt, \
+            xycoords='axes fraction', xytext=centerPt,\
+            textcoords='axes fraction', va='center', \
+            ha="center", bbox=nodeType, arrowprops=arrow_args)
+    
+def createPlot():
+    fig = plt.figure(1, facecolor='white')
+    fig.clf()
+    createPlot.ax1 = plt.subplot(111, frameon=False)
+    plotNode('a decison node', (0.5, 0.1), (0.1, 0.5), decisionNode)
+    plotNode('a decision leaf', (0.8, 0.1), (0.3, 0.8), leafNode)
+    plt.show()
+
+def getNumLeafs(myTree):
+    numLeafs = 0
+    firstStr = myTree.keys()[0]
+    secondDict = myTree[firstStr]
+    for key in secondDict.keys():
+        if type(secondDict[key]).__name__ == 'dict':
+            numLeafs += getNumLeafs(secondDict[key])
+        else:
+            numLeafs += 1
+    return numLeafs
+
+def getTreeDepth(myTree):
+    maxdepth = 0
+    thisDepth = 0
+    firstStr = myTree.keys()[0]
+    secondDict = myTree[firstStr]
+    for key in secondDict.keys():
+        if type(secondDict[key]).__name__ == 'dict':
+            thisDepth = 1 + getTreeDepth(secondDict[key])
+        else:
+            thisDepth += 1
+        if thisDepth > maxdepth:
+            maxdepth = thisDepth
+    return maxdepth
+
+def retrieveTree(i):
+    listOfTrees=[
+            {'no surfacing':{0:'no', 1:{'flippers':{0:'no', 1:'yes'}}}},
+            {'no surfacing':{0:'no', 1:{'flippers':{0:{'haed':{0:'no', 1:'yes'}}, 1:'no'}}}}
+            ]
+    return listOfTrees[i]
+
+def plotMidText(cntrPt, parentPt, txtString):
+    xMid = (parentPt[0]-cntrPt[0])/2.0 + cntrPt[0]
+    yMid = (parentPt[1]-cntrPt[1])/2.0 + cntrPt[1]
+    createPlot.ax1.text(xMid, yMid, txtString, va='center', ha='center', rotation=30)
+
+def plotTree(myTree, parentPtr, nodeTxt):
+    numLeafs = getNumLeafs(myTree)
+    depth = getTreeDepth(myTree)
+    firstStr = myTree.keys()[0]
+    cntrPt = (plotTree.xOff + (1.0 + float(numLeafs))/2.0/plotTree.totalW, plotTree.yOff)
+
+
