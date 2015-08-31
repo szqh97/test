81cdb8579d86a52a04050b2d1e13f6828ed04c22
szqh97 <szqh97@163.com>
2015-08-31 08:57:17 +0800

    add func call

--- rust/func_call.rs
+++ rust/func_call.rs
@@ -21,9 +21,24 @@ impl Bar for Baz {
 
 }
 
+macro_rules! foo {
+    (x=> $e:expr) => (
+        println!("mode X: {}", $e)
+    );
+    (y=> $e:expr) => (
+        println!("mode y: {}", $e)
+    );
+}
+macro_rules! five_time {
+    ($x:expr) => (5*$x);
+}
 fn main() {
     let b = Baz;
     Foo::f(&b);
     Bar::f(&b);
+    foo!(y=>3);
+    foo!(x=>3);
+
+    println!(five_time!(2 + 4));
 }
 
