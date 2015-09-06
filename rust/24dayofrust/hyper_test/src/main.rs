80b0d9a529ca9ad17303460c0a284bcfac3a0573
szqh97 <szqh97@163.com>
2015-09-06 23:28:03 +0800

    add hyper test code

---
+++ rust/24dayofrust/hyper_test/src/main.rs
@@ -0,0 +1,32 @@
+extern crate hyper;
+use std::io::Read;
+use hyper::{Client};
+
+
+
+fn get_content(url: &str)-> hyper::Result<String> {
+    let client = Client::new();
+    let mut response = try!(client.get(url).send());
+    let mut buf = String::new();
+    try!(response.read_to_string(&mut buf));
+    Ok(buf)
+
+    
+}
+fn main() {
+    /*
+    let client = Client::new();
+    let url = "http://www.baidu.com";
+    let mut response = match client.get(url).send() {
+        Ok(response) => response,
+        Err(_) => panic!("Broke up"),
+    };
+    let mut buf = String::new();
+    match response.read_to_string(&mut buf) {
+        Ok(_) => (),
+        Err(_) => panic!("I gave up"),
+    };
+    println!("buf: {}", buf);
+    */
+    println!("buf: {:?}", get_content("http://www.baidu.com"));
+}
