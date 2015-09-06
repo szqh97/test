0d2f37fb4c8e27525cdcb7ae99cc477ab03e7352
szqh97 <szqh97@163.com>
2015-09-06 23:16:33 +0800

    add docopt test code

--- rust/24dayofrust/docopt/src/main.rs
+++ rust/24dayofrust/docopt/src/main.rs
@@ -2,20 +2,20 @@ extern crate rustc_serialize;
 extern crate docopt;
 
 use docopt::Docopt;
-use 
 
-static USAGE: &'static str =
-"Usage: wc [options] [<file>]\
-Options:\
-    -c, --bytes  print the byte counts\
-    -m, --chars  print the character counts\
-    -l, --lines  print the newline counts\
-    -w, --words  print the word counts\
-    -L, --max-line-length  print the length of the longest line\
-    -h, --help  display this help and exit\
-    -v, --version  output version information and exit";
+static USAGE: &'static str =r#"Usage: wc [options] [<file>]
+
+Options:
+    -c, --bytes  print the byte counts
+    -m, --chars  print the character counts
+    -l, --lines  print the newline counts
+    -w, --words  print the word counts
+    -L, --max-line-length  print the length of the longest line
+    -h, --help  display this help and exit
+    -v, --version  output version information and exit"#;
 
 #[derive(RustcDecodable)]
+#[derive(Debug)]
 struct Args {
     arg_file: Option<String>,
     flag_bytes: bool,
@@ -26,5 +26,24 @@ struct Args {
 
 fn main() {
     let args:Args = Docopt::new(USAGE).and_then(|d| d.decode()).unwrap_or_else(|e| e.exit());
+    let docopt = match Docopt::new(USAGE) {
+        Ok(d) => d,
+        Err(e) => e.exit(),
+    };
+    //println!("{:?}", docopt);
+
+    let args:Args = match docopt.decode() {
+         Ok(args) => args,
+         Err(e) => e.exit(),
+    };
+
+    //println!("Counting stuff in {}", args.arg_file.unwrap_or("standard input").to_string());
+    if args.flag_bytes {
+        println!("Counting bytes");
+    }
+    if args.flag_chars {
+        println!("Counting characters!");
+    }
+
 
 }
