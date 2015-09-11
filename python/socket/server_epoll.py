5efa46aa4a6183714712d17c0a7cb6d8d7c74cba
szqh97 <szqh97@163.com>
2015-09-11 16:52:03 +0800

    add server_epoll.py

--- python/socket/server_epoll.py
+++ python/socket/server_epoll.py
@@ -7,7 +7,7 @@ EOL1 = b'\n\n'
 EOL2 = b'\n\r\n'
 response = b'HTTP/1.0 200 OK\r\nDate: Mon, 1 Jan 1996 01:01:01 GMT\r\n'
 response += b'Content-Type: text/plain\r\nContent-Length: 13\r\n\r\n'
-response += b'Hello, world!'
+response += b'Hello, world!\n\n'
 
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -26,6 +26,7 @@ try:
         events = epoll.poll(1)
         for fileno, event in events:
             if fileno == serversocket.fileno():
+                print('fileno, {}'.format(fileno))
                 connection, address = serversocket.accept()
                 connection.setblocking(0)
                 epoll.register(connection.fileno(), select.EPOLLIN)
@@ -33,12 +34,26 @@ try:
                 requests[connection.fileno()] = b''
                 responses[connection.fileno()] = response
             elif event & select.EPOLLIN:
+                print('EPOLLIN')
                 requests[fileno] += connections[fileno].recv(1024)
                 if EOL1 not in requests[fileno] and EOL2 not in requests[fileno]:
                     epoll.modify(fileno, select.EPOLLOUT)
-
-
+                    print('-'*40 + '\n' + requests[fileno].decode()[:-2])
+            elif event & select.EPOLLOUT:
+                print('EPOLLOUT')
+                byteswriten = connections[fileno].send(responses[fileno])
+                responses[fileno] = responses[fileno][byteswriten:]
+                if len(responses[fileno]) == 0:
+                    epoll.modify(fileno, 0)
+                    connections[fileno].shutdown(socket.SHUT_RDWR)
+            elif event & select.EPOLLHUP:
+                print('EPOLLHUP')
+                epoll.unregister(fileno)
+                connections[fileno].close()
+                del connections[fileno]
 except Exception, e:
     raise e
 finally:
-    pass
+    epoll.unregister(serversocket.fileno())
+    epoll.close()
+    serversocket.close()
