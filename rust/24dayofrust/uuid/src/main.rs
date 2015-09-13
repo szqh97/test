ff56d4668d010b6ab59d3d009ff59b468fd5c1ac
szqh97 <szqh97@163.com>
2015-09-14 07:13:23 +0800

    update

--- rust/24dayofrust/uuid/src/main.rs
+++ rust/24dayofrust/uuid/src/main.rs
@@ -9,4 +9,8 @@ fn main() {
         println!("{}", u.to_string());
         println!("{}", u.to_hyphenated_string());
     }
+    println!("{:?}", Uuid::parse_str("d27cdb6e-ae6d-11cf-96b8-44455354000"));
+    println!("{:?}", Uuid::parse_str("x27cdb6e-ae6d-11cf-96b8-444553540000"));
+    println!("{:?}", Uuid::parse_str("d27cdb6-eae6d-11cf-96b8-444553540000"));
+    println!("{:?}", Uuid::parse_str("d27cdb6e-ae6d-11cf-96b8-444553540000"));
 }
