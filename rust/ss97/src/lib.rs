6d5fe7ade4cc04294f678e2cef26169983a27d06
szqh97 <szqh97@163.com>
2015-09-15 07:48:46 +0800

    update

--- rust/ss97/src/lib.rs
+++
@@ -1,4 +0,0 @@
-#[macro_use]
-extern crate log;
-mod shell;
-
--- rust/ss97/src/main.rs
+++ rust/ss97/src/main.rs
@@ -1,6 +1,12 @@
-extern crate shell;
+#[macro_use]
+extern crate log;
+extern crate env_logger;
+mod shell;
 use shell::test_log;
 fn main() {
+    env_logger::init().unwrap();
     println!("Hello, world!");
     test_log();
+    info!("starting..");
+    warn!("warax");
 }
