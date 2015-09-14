34c2e27bccbd6b41501816faf0e609d5f4087b73
szqh97 <szqh97@163.com>
2015-09-15 07:59:43 +0800

    update shell.rs

--- rust/ss97/src/shell.rs
+++ rust/ss97/src/shell.rs
@@ -1,12 +1,12 @@
 //#[macro_use]
 //extern crate log;
 
-    pub fn test_log() {
-        println!("xxxxxxxxxx");
-        info!("test log info.");
-        debug!("test log info.");
-        error!("test log info.");
-        println!("xxxxxxxxxx");
+pub fn test_log() {
+    println!("xxxxxxxxxx");
+    info!("test log info.");
+    debug!("test log info.");
+    error!("test log info.");
+    println!("xxxxxxxxxx");
 
-    }
+}
 
