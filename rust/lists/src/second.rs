2b443ed4851614fb35e9a3248faac1dcc1c68f46
szqh97 <szqh97@163.com>
2015-09-28 08:04:56 +0800

    update seconde.rs

--- rust/lists/src/second.rs
+++ rust/lists/src/second.rs
@@ -62,9 +62,49 @@ impl<T> Drop for List<T>{
     }
 }
 
+pub trait Iterator{
+    type Item;
+    fn next(&mut self) -> Option<Self::Item>; 
+}
+
+pub struct IntoIter<T>(List<T>); 
+
+impl<T> List<T>{
+    pub fn into_iter(self) -> IntoIter<T> {
+        IntoIter(self)
+    }
+}
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.pop()
+    }
+}
+
+pub struct Iter<'a, T> {
+    next: Option<&'a Node<T>>,
+}
+
+impl<'a, T> List<T> {
+    pub fn iter(&'a self) -> Iter<'a, T> {
+        Iter{next:self.head.map(|node| &*node)}
+    }
+}
+
+impl<'a, T> Iterator for Iter<'a, T>{
+    type Item = &'a T;
+    fn next(&'a mut self) -> Option<Self::Item> {
+        self.next.map(|node|{
+            self.next = node.next.map(|node| &'a *node);
+            &'a node.elem
+        })
+    }
+}
 #[cfg(test)]
 mod test{
     use super::List;
+    use second::Iterator;
 
     #[test]
     fn basic() {
@@ -86,5 +126,17 @@ mod test{
 
         assert_eq!(list.pop(), Some(1));
         assert_eq!(list.pop(), None);
+    }
+
+    #[test]
+    fn into_iter() {
+        let mut list = List::new();
+        list.push(1);
+        list.push(2);
+        list.push(3);
+        let mut iter = list.into_iter();
+        assert_eq!(iter.next(), Some(3));
+        assert_eq!(iter.next(), Some(2));
+        assert_eq!(iter.next(), Some(1));
     }
 }
