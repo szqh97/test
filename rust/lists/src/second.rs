0fb1657dcbdc41982da8b31221e847a7e6cd4177
szqh97 <szqh97@163.com>
2015-09-29 22:53:32 +0800

    update

--- rust/lists/src/second.rs
+++ rust/lists/src/second.rs
@@ -82,22 +82,22 @@ impl<T> Iterator for IntoIter<T> {
     }
 }
 
-pub struct Iter<'a, T> {
+pub struct Iter<'a, T: 'a> {
     next: Option<&'a Node<T>>,
 }
 
-impl<'a, T> List<T> {
-    pub fn iter(&'a self) -> Iter<'a, T> {
-        Iter{next:self.head.map(|node| &*node)}
+impl<T> List<T> {
+    pub fn iter<'a>(&'a self) -> Iter<'a, T> {
+        Iter{next:self.head.as_ref().map(|node| &**node)}
     }
 }
 
 impl<'a, T> Iterator for Iter<'a, T>{
     type Item = &'a T;
-    fn next(&'a mut self) -> Option<Self::Item> {
+    fn next(& mut self) -> Option<Self::Item> {
         self.next.map(|node|{
-            self.next = node.next.map(|node| &'a *node);
-            &'a node.elem
+            self.next = node.next.as_ref().map(|node| & **node);
+            & node.elem
         })
     }
 }
