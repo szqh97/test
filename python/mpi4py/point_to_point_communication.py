0bc427eedec7ae06e41a62584b7dd346de474a00
szqh97 <szqh97@163.com>
2015-10-19 10:11:13 +0800

    add mpi4py

---
+++ python/mpi4py/point_to_point_communication.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import mpi4py.MPI as MPI
+
+comm = MPI.COMM_WORLD
+comm_rank = comm.Get_rank()
+comm_size = comm.Get_size()
+
+data_send = [comm_rank] * 5
+comm.send(data_send, dest=(comm_rank+1) % comm_size)
+data_recv = comm.recv(source=(comm_rank-1) %comm_size)
+
+print ('my rank is %d, and I receiveed:' % comm_rank)
+print data_recv
