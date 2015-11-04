eb2f1d738750883b14f66a1441edc21f6395d257
szqh97 <szqh97@163.com>
2015-11-04 22:25:47 +0800

    update

--- rust/cursor_test.rs
+++ rust/cursor_test.rs
@@ -1,20 +1,17 @@
 use std::io::prelude::*;
-use std::io::{self, SeekFrom};
-use std::result::Result;
+use std::io::{self,Result, SeekFrom};
 use std::fs::File;
-
 
 fn write_ten_bytes_at_end<W: Write + Seek>(writer: &mut W) -> Result<()> {
     try!(writer.seek(SeekFrom::End(-10)));
     for i in 0..10{
         try!(writer.write(&[i]));
     }
-
     Ok(())
 }
 
 pub fn main() {
     let mut file = try!(File::create("foo.txt"));
-    try!(write_ten_bytes_at_end(&mut file));
+    //try!(write_ten_bytes_at_end(&mut file));
     
 }
