16c60ec73bd5ab3aa307ec111b4e7a18fb03f547
szqh97 <szqh97@163.com>
2015-11-05 07:50:50 +0800

    add err1.rs option test

--- rust/err_handling/err1.rs
+++ rust/err_handling/err1.rs
@@ -8,13 +8,34 @@ fn guess(n: i32) -> bool {
     n == 5
 }
 
-pub fn main() {
-    //guess(11);
-    
+fn arg_test() {
+
     let mut argv = env::args();
     let arg: String = argv.nth(1).unwrap();
     let n: i32 = arg.parse().unwrap();
     println!("{}", 2 * n);
+
+}
+
+fn find(haystack: &str, needle: char) -> Option<usize> {
+    for (offset, c) in haystack.char_indices(){
+        if c == needle{
+            return Some(offset)
+        }
+    }
+    None
+    
+}
+pub fn main() {
+    //guess(11);
+    
+    //arg_test();
+    
+   let file_name = "foobar.rs";
+   match find(file_name, '.'){
+       None => println!("No file extension found."),
+       Some(i) => println!("File extension:{}", &file_name[i+1..]),
+   }
 
 
 }
