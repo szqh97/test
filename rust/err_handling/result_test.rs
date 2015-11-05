08ea45431447158ca67b1230dbd9ef240370b98b
szqh97 <szqh97@163.com>
2015-11-05 21:56:44 +0800

    update

--- rust/err_handling/result_test.rs
+++ rust/err_handling/result_test.rs
@@ -1,4 +1,5 @@
 use std::num::ParseIntError;
+use std::env;
 
 fn double_number_(number_str: &str) -> i32 {
     2 * number_str.parse::<i32>().unwrap()
@@ -15,6 +16,12 @@ fn double_number2(number_str: &str) -> Result<i32, ParseIntError> {
     number_str.parse::<i32>().map(|n| 2*n)
 }
 
+fn double_arg(mut argv: env::Args) -> Result<i32, String> {
+    argv.nth(1)
+        .ok_or("Please give at leaset one argument".to_owned())
+        .and_then(|arg| arg.parse::<i32>().map_err(|err| err.to_string()))
+}
+
 
 pub fn main() {
     match double_number("10a") {
@@ -25,5 +32,10 @@ pub fn main() {
     match double_number2("10a") {
         Ok(n) => assert_eq!(n, 20),
         Err(err) => println!("Error: {:?}", err),
+    }
+
+    match double_arg(env::args()) {
+        Ok(n) => println!("{}", n),
+        Err(error) => println!("Error: {}", error),
     }
 }
