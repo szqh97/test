5536bf29db652eff1df684d9155093ddd005c165
szqh97 <szqh97@163.com>
2015-11-10 23:25:20 +0800

    update

--- rust/rust-zookeeper-test/examples/test.rs
+++ rust/rust-zookeeper-test/examples/test.rs
@@ -4,16 +4,33 @@ use std::collections::HashMap;
 use std::sync::mpsc;
 use zookeeper::listeners;
 
-use zookeeper::io;
+use zookeeper::watch;
+use zookeeper::{Watcher, WatchedEvent};
 
+use zookeeper::io;
 
 pub fn test_io() {
     let (addrs, chroot) = zookeeper::ZooKeeper::parse_connect_string("127.0.0.1:2181, ::1:2181/mesos").unwrap();
     println!("length {}", addrs.len());
     println!("{:?}", addrs);
+    println!("{:?}", chroot);
     let mut hosts = zookeeper::io::Hosts::new(addrs);
     let addr = hosts.get();
     println!("{:?}", addr);
+    
+}
+
+struct LoggingWatcher;
+impl Watcher for LoggingWatcher {
+    fn handle(&mut self, e: &WatchedEvent) {
+        //info!("{:?}", e)
+        println!("{:?}", e)
+    }
+}
+
+pub fn test_watch() {
+    let chroot = "mesos";
+    let zkw = watch::ZkWatch::new(chroot, LoggingWatcher);
     
 }
 
