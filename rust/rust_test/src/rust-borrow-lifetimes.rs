fb97f849b2a16d442c744a1249e237b94036ca41
szqh97 <szqh97@163.com>
2015-11-25 18:49:51 +0800

    add rust borrow lifetime

---
+++ rust/rust_test/src/rust-borrow-lifetimes.rs
@@ -0,0 +1,38 @@
+#![feature(box_syntax)] 
+struct Foo {
+    f: Box<i32>,
+}
+
+fn main() {
+    /*
+     * #1 
+    let mut a = Foo{f: box 0};
+    //let x = &mut a;
+    // cannot borrow a.f as immutable because a is also borrowed as x
+    //println!("{}", a.f);
+    //
+    */
+
+    /*
+     *
+    let mut a = Foo{f: box 0};
+
+    {
+
+        let x = &mut a;
+    }
+    println!("{}", a.f);
+    */
+
+    /*
+    let mut a = Foo {f: box 0};
+    let x = &mut a;
+    let y = x;
+
+    // error 
+    //println!("{}", x.f);
+    */
+
+
+
+}
