4fe72b39702abe020824962d5c61c9136b7acbd8
szqh97 <szqh97@163.com>
2015-12-04 22:31:04 +0800

    update

--- rust/phrases/src/main.rs
+++ rust/phrases/src/main.rs
@@ -1,6 +1,7 @@
 extern crate phrases;
+use phrases::english::greetings;
 fn main() {
-    println!("Hello in English: {}", phrases::english::greetings::hello());
+    println!("Hello in English: {}", greetings::hello());
     println!("Goodbye in English: {}", phrases::english::farewells::goodbye());
     
     println!("Hello in Japanese: {}", phrases::japanese::greetings::hello());
