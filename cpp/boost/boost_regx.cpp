d896b827bc8bdfe9cd09f74b8efef293beb04fbe
szqh97 <szqh97@163.com>
2016-03-18 17:01:05 +0800

    update

--- cpp/boost/boost_regx.cpp
+++ cpp/boost/boost_regx.cpp
@@ -6,9 +6,10 @@ int main(int argc, char *argv[])
 {
     boost::smatch matches;
 
-    string seedurl = "http://aaaaletorrenty.pl/download.php?id=c4d8ce7b45295e2a7028c60b8b390a6319a32a2a&f=FutileStruggles+Brielleas+Blackmail+Blunder-Part+1+HD+%5BHD-720p%5D.torrent";
+    //string seedurl = "http://aaaaletorrenty.pl/download.php?id=c4d8ce7b45295e2a7028c60b8b390a6319a32a2a&f=FutileStruggles+Brielleas+Blackmail+Blunder-Part+1+HD+%5BHD-720p%5D.torrent";
+    string seedurl = "http://itorrents.org/torrent/7E78D74BDCE89A8EA2F2A98D560F833BA6042727.torrent?title=%5BKaTorrents.space%5Dnurarihyon.no.mago.nura.rise.of.the.yokai.clan.dual.audio.720p";
     //seedurl = "ABCD";
-    boost::regex reg("[a-fA-F0-9]{40, 40}");
+    boost::regex reg("[a-fA-F0-9]{40, 40}", boost::regex::perl);
     //boost::regex reg("A.C", boost::regex_constants::perl);
     //string reg = "[a-fA-F0-9]";
     bool b = boost::regex_search(seedurl, matches, reg);
--- cpp/boost/test_regex.cpp
+++ cpp/boost/test_regex.cpp
@@ -4,7 +4,8 @@ using namespace std;
 
 int main( int argc, char * argv[] )
 {
-    string exp = "Key   Peele";
+    //string exp = "Key   Peele";
+    string exp = "The Game Key.and.        his.girlfriend;.Keegan.and.Jordan.                            havin.2014.HDTV.XviD-LOL[VTV].mp4";
     exp = string(" ") + exp + " ";
     printf("exp 1 [%s]\n", exp.c_str());
 
@@ -13,12 +14,17 @@ int main( int argc, char * argv[] )
     //}
     printf("exp 2 [%s]\n", exp.c_str());
 
+    boost::regex r("[\\s]+", boost::regex::perl);
+    exp = boost::regex_replace(exp, r, " ");
+
     boost::algorithm::replace_all(exp, " ", ".*");
     printf("exp 3 [%s]\n", exp.c_str());
  
-    boost::regex reg(exp, boost::regex::icase);
+    boost::regex reg(exp, boost::regex::icase| boost::regex::sed);
     boost::cmatch m;
-    string dstr = "Key.and.Peele.Season.3.Episode.13.Keegan.gets.distracted.while.on.the.phone.with.his.girlfriend;.Keegan.and.Jordan.havin.2014.HDTV.XviD-LOL[VTV].mp4";
+
+    //string dstr = "Key.and.Peele.Season.3.Episode.13.Keegan.gets.distracted.while.on.the.phone.with.his.girlfriend;.Keegan.and.Jordan.havin.2014.HDTV.XviD-LOL[VTV].mp4";
+    string dstr = "The Game Key.and.        his.girlfriend;.Keegan.and.Jordan.                            havin.2014.HDTV.XviD-LOL[VTV].mp4";
     printf("str  [%s]\n", dstr.c_str());
 
     if(boost::regex_match(dstr.c_str(), m, reg)){
@@ -27,5 +33,28 @@ int main( int argc, char * argv[] )
     else {
         printf("not match\n");
     }
+
+
+
+    cout << "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX" << endl;
+
+    //string e = "The.*Game.*Key.and..*his.girlfriend;.Keegan.and.Jordan..*havin.2014.HDTV.XviD-LOL[VTV].mp4";
+    //string  e= "The Game Key.and.        his.girlfriend;.Keegan.and.Jordan.                            havin.2014.HDTV.XviD-LOL[VTV].mp4 ";
+    //string e= ".*The.*Game.*Key\\.and\\..*his\\.girlfriend;\\.Keegan\\.and\\.Jordan\\..*havin\\.2014\\.HDTV\\.XviD-LOL[VTV]\\.mp4.*";
+    //string dst = " The Game Key.and.        his.girlfriend;.Keegan.and.Jordan.                            havin.2014.HDTV.XviD-LOL[VTV].mp4 ";
+
+    string e= ".*The.*Game.*Key.and..*his.girlfriend;.Keegan.and.Jordan..*havin.\\{\\}2014.HDTV.XviD-\\(\\)LOL\\[VTV\\].mp?4?+.*";
+    string dst = "The Game Key.and.        his.girlfriend;.Keegan.and.Jordan.                            havin.{}2014.HDTV.XviD-()LOL[VTV].mp?4?+";
+    boost::regex rr(e, boost::regex::perl);
+
+    boost::smatch sm;
+    if (boost::regex_match(dst, sm, rr))
+    {
+        cout << "match" << endl;
+    }
+    else {
+        cout << "no match" << endl;
+    }
+    cout << sm.size() << endl;
 }
 
