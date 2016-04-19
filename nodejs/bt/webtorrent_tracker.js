fbb4e54baa31fd9ff1cbed98ce037a7524334137
szqh97 <szqh97@163.com>
2016-04-19 10:41:12 +0800

    test webtorrent

--- nodejs/bt/webtorrent_tracker.js
+++ nodejs/bt/webtorrent_tracker.js
@@ -1,22 +1,38 @@
 #!/usr/bin/env node
 var WebTorrent = require('webtorrent')
 var opts = {
-  announce: [   
-    'wss://tracker.btorrent.xyz',
-  'wss://tracker.fastcast.nz',
-  'wss://tracker.openwebtorrent.com',
-  'wss://tracker.webtorrent.io',
-  'ws://wstracker.vuze.com:8000/'
-  ],
+    announce: [   
+        'wss://tracker.btorrent.xyz',
+    'wss://tracker.fastcast.nz',
+    'wss://tracker.openwebtorrent.com',
+    'wss://tracker.webtorrent.io',
+    'ws://wstracker.vuze.com:8000/'
+        ],
 }
 
 var torrentId = process.argv.splice(2)[0]
 var client = new WebTorrent();
-var torrent = client.add(torrentId, opts)
+var torrent = client.add(torrentId, opts);
 console.log(torrent);
 torrent.on('wire', function (wire, addr){
     console.log('conected to peer with address:',addr )
-})
+});
 
-setTimeout(function () { process.exit(0) }, 1000)
+
+server = torrent.createServer();
+function initServer () {
+    if (torrent.ready) onReady()
+    else torrent.once('ready', onReady)
+};
+
+function onReady(){
+    drawTorrent(torrent)
+}
+
+function drawTorrent(torrent){
+    console.log(torrent)
+}
+
+
+setTimeout(function () { process.exit(0) }, 1000);
 
