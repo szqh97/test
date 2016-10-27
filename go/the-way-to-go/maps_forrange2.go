3402dc5ec1d8f32982bf54e2f7968636725e9ba1
li_yun <liyun@idongjia.cn>
2016-10-27 09:59:57 +0800

    update map slices

---
+++ go/the-way-to-go/maps_forrange2.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	items := make([]map[int]int, 5)
+	for i := range items {
+		items[i] = make(map[int]int, 1)
+		items[i][1] = 2
+	}
+	fmt.Println(items)
+
+	items2 := make([]map[int]int, 5)
+	for _, item := range items2 {
+		item = make(map[int]int, 1)
+		item[1] = 2
+	}
+	fmt.Println(items2)
+}
