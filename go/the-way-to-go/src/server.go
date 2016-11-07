c654d6cca23c6bba6fa71fe0e70658c0418a51be
li_yun <liyun@idongjia.cn>
2016-11-07 09:06:55 +0800

    update

---
+++ go/the-way-to-go/src/server.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func main() {
+	fmt.Println("Starting the server...")
+	listener, err := net.Listen("tcp", "localhost:50000")
+	if err != nil {
+		fmt.Println("Err listening", err.Error())
+		return
+	}
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error Accepting", err.Error())
+			return
+		}
+		go doServerStuff(conn)
+	}
+}
+
+func doServerStuff(conn net.Conn) {
+	for {
+		buf := make([]byte, 512)
+		_, err := conn.Read(buf)
+		if err != nil {
+			fmt.Println("Error reading", err.Error())
+			return
+		}
+		fmt.Printf("Received data: %v", string(buf))
+	}
+}
---
+++ go/the-way-to-go/src/timer_goroutine.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	tick := time.Tick(1e8)
+	boom := time.After(5e8)
+
+	for {
+		select {
+		case <-tick:
+			fmt.Println("tick.")
+		case <-boom:
+			fmt.Println("Boom")
+			return
+		default:
+			fmt.Println("    .")
+			time.Sleep(5e7)
+
+		}
+	}
+}
