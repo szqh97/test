a5d756d35243c9262c581dbc2de91328dedd6579
yun <liyun@idongjia.cn>
2016-11-10 23:00:20 +0800

    update

--- go/t/poll_url.go
+++ go/t/poll_url.go
@@ -1,18 +1,21 @@
 package main
+
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
 )
+
 var urls = []string{
-    "http://google.com/",
-    "http://baidu.com/",
+	"http://google.com/",
+	"http://baidu.com/",
 }
+
 func main() {
-    for _, url := range urls {
-        resp, err := http.Head(url)
-        if err != nil {
-            fmt.Println("Error:", url, err)
-        }
-        fmt.Print(url, ": ", resp.Status)
-    }
+	for _, url := range urls {
+		resp, err := http.Head(url)
+		if err != nil {
+			fmt.Println("Error:", url, err)
+		}
+		fmt.Print(url, ": ", resp.Status)
+	}
 }
