2d793c8c6320b797478134ca5758e21479c9b4f6
yun <liyun@idongjia.cn>
2017-08-21 08:35:50 +0800

    update

--- go/src/gopl/ch8/du4.go
+++ go/src/gopl/ch8/du4.go
@@ -1,21 +1,34 @@
 package main
 
 import (
-	"flag"
 	"fmt"
-	"io/ioutil"
 	"os"
 	"path/filepath"
 	"sync"
 	"time"
 )
 
-func printDsikUsage(nfiles, nbytes int64) {
-	fmt.Printf("%d files %1.f GB\n", nfiles, float64(nbytes)/1e9)
+var done = make(chan struct{})
+
+func cancelled() bool {
+	select {
+	case <-done:
+		return true
+	default:
+		return false
+	}
+}
+
+func printDiskUsage(nfiles, nbytes int64) {
+	fmt.Printf("%d file %.1f GB\n", nfiles, float64(nbytes)/1e9)
 }
 
 func walkDir(dir string, n *sync.WaitGroup, fileSizes chan<- int64) {
 	defer n.Done()
+	if cancelled() {
+		return
+	}
+
 	for _, entry := range dirents(dir) {
 		if entry.IsDir() {
 			n.Add(1)
@@ -30,30 +43,37 @@ func walkDir(dir string, n *sync.WaitGroup, fileSizes chan<- int64) {
 var sema = make(chan struct{}, 20)
 
 func dirents(dir string) []os.FileInfo {
-	sema <- struct{}{}
+	select {
+	case sema <- struct{}{}:
+	case <-done:
+		return nil
+	}
 	defer func() { <-sema }()
-
-	entries, err := ioutil.ReadDir(dir)
+	f, err := os.Open(dir)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "du: %v\n", err)
 		return nil
 	}
+	defer f.Close()
+
+	entries, err := f.Readdir(0)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "du:%v\n", err)
+	}
 	return entries
 }
-
-var vFlag = flag.Bool("v", false, "show verbose progress")
-
 func main() {
-	flag.Parse()
-	roots := flag.Args()
+	roots := os.Args[1:]
 	if len(roots) == 0 {
 		roots = []string{"."}
 	}
+	go func() {
+		os.Stdin.Read(make([]byte, 1))
+		close(done)
+	}()
 
 	fileSizes := make(chan int64)
-
 	var n sync.WaitGroup
-
 	for _, root := range roots {
 		n.Add(1)
 		go walkDir(root, &n, fileSizes)
@@ -64,15 +84,15 @@ func main() {
 		close(fileSizes)
 	}()
 
-	var tick <-chan time.Time
-
-	if *vFlag {
-		tick = time.Tick(500 * time.Millisecond)
-	}
+	tick := time.Tick(500 * time.Millisecond)
 	var nfiles, nbytes int64
 loop:
 	for {
 		select {
+		case <-done:
+			for range fileSizes {
+			}
+			return
 		case size, ok := <-fileSizes:
 			if !ok {
 				break loop
@@ -80,8 +100,9 @@ loop:
 			nfiles++
 			nbytes += size
 		case <-tick:
-			printDsikUsage(nfiles, nbytes)
+			printDiskUsage(nfiles, nbytes)
 		}
 	}
-	printDsikUsage(nfiles, nbytes)
+
+	printDiskUsage(nfiles, nbytes)
 }
